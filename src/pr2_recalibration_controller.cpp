6f7602cbc8a230e95718420884a6f791b5667e39
Rahul Udasi <rahuludasi@hotmail.com>
2012-09-25 10:35:27 -0700

    add dereks changes

--- src/pr2_recalibration_controller.cpp
+++ src/pr2_recalibration_controller.cpp
@@ -39,63 +39,116 @@ void Pr2RecalibrationValues::starting()
 
 bool Pr2RecalibrationValues::getOffset(pr2_recalibration_values::GetCalibrationOffset::Request& req, pr2_recalibration_values::GetCalibrationOffset::Response& resp)
 {
-  int num_elements = robot_->model_->transmissions_.size();
-  ROS_INFO("number of transmissions %d",num_elements);
-
-  for(int i = 0; i < num_elements; i++)
+  if (robot_ ==NULL)
   {
-     pr2_recalibration_values::values temp;
-     std::string my_transmission_name = robot_->model_->transmissions_[i]->name_;
-     if(my_transmission_name.compare("r_gripper_trans") != 0 && my_transmission_name.compare("l_gripper_trans") != 0)
-     {
-        int num_actuators = robot_->model_->transmissions_[i]->actuator_names_.size();
-        ROS_INFO("number of actuators %d", num_actuators);
-        ROS_INFO("transmission name is %s",my_transmission_name.c_str()); 
+    ROS_ERROR("robot is null");
+    return false;
+  }
+  if (robot_->model_ == NULL)
+  {
+    ROS_ERROR("robot model is null");
+    return false;
+  }
 
-        std::vector<pr2_mechanism_model::JointState*> temp_joint_ptr;
-        std::vector<pr2_hardware_interface::Actuator*> temp_actuator_ptr;
-   
-        for(int j = 0; j < num_actuators; j++)
-        {
-           std::string my_actuator_name = robot_->model_->transmissions_[i]->actuator_names_[j];
-           ROS_INFO("Actuator name is %s",my_actuator_name.c_str());
-           pr2_hardware_interface::Actuator* my_actuator = robot_->model_->getActuator(my_actuator_name);
+  int num_transmissions = robot_->model_->transmissions_.size();
+  ROS_INFO("number of transmissions %d",num_transmissions);
 
-	   pr2_mechanism_model::JointState* temp_joint = new pr2_mechanism_model::JointState;
-	   pr2_hardware_interface::Actuator* temp_actuator = new pr2_hardware_interface::Actuator(*my_actuator);
-	   temp_joint_ptr.push_back(temp_joint);
+  for(int i = 0; i < num_transmissions; i++)
+  {
+    pr2_mechanism_model::Transmission *transmission = robot_->model_->transmissions_[i];
 
-	   temp_actuator->state_.position_ = my_actuator->state_.zero_offset_;
-	   temp_actuator_ptr.push_back(temp_actuator);
+    pr2_recalibration_values::values temp_values;
+    std::string my_transmission_name = transmission->name_;
+    temp_values.transmission_name = my_transmission_name;
+    if( (my_transmission_name.compare("r_gripper_trans") == 0) ||
+	(my_transmission_name.compare("l_gripper_trans") == 0) ||
+	(my_transmission_name.compare("torso_lift_trans") == 0) )
+    {
+      ROS_WARN("Skipping transmision %s", my_transmission_name.c_str());
+      continue;
+    }
 
+    ROS_INFO("Transmission name is %s",my_transmission_name.c_str());
 
-           temp.actuator_name.push_back(my_actuator_name); 
-	
-	   //temp_joint_ptr.erase(temp_joint_ptr.begin());
-	   //temp_actuator_ptr.erase(temp_actuator_ptr.begin());
+    bool ok = true;
 
-	   //delete temp_joint;
-	   //delete temp_actuator;
-        }
+    int num_actuators = transmission->actuator_names_.size();
+    ROS_INFO(" Number of actuators %d", num_actuators);
+    std::vector<pr2_hardware_interface::Actuator*> temp_actuator_list;
+    for(int j = 0; j < num_actuators; j++)
+    {
+      std::string my_actuator_name = transmission->actuator_names_[j];
+      ROS_INFO(" Actuator name is %s",my_actuator_name.c_str());
+      pr2_hardware_interface::Actuator* my_actuator = robot_->model_->getActuator(my_actuator_name);
+      if(my_actuator == NULL)
+      {
+	ROS_ERROR("Could not find actuator: %s", my_actuator_name.c_str());
+	ok = false;
+	break;
+      }
+      else
+      {
+	pr2_hardware_interface::Actuator* temp_actuator = new pr2_hardware_interface::Actuator(*my_actuator);
+	temp_actuator->state_.position_ = my_actuator->state_.zero_offset_;
+	temp_actuator_list.push_back(temp_actuator);
+	temp_values.actuator_name.push_back(my_actuator_name);
+      }
+    }
+    int num_joints = transmission->actuator_names_.size();
+    ROS_INFO(" Number of joints %d", num_joints);
+    std::vector<pr2_mechanism_model::JointState*> temp_joint_list;
+    for(int j = 0; j < num_joints; j++)
+    {
+      std::string my_joint_name = transmission->joint_names_[j];
+      ROS_INFO(" Joint name is %s",my_joint_name.c_str());
+      pr2_mechanism_model::JointState* my_joint = robot_->getJointState(my_joint_name);
+      if(my_joint == NULL)
+      {
+	ROS_ERROR("Could not find joint: %s", my_joint_name.c_str());
+	ok = false;
+	break;
+      }
+      else
+      {
+	pr2_mechanism_model::JointState* temp_joint = new pr2_mechanism_model::JointState(*my_joint);
+	temp_joint_list.push_back(temp_joint);
+	temp_values.joint_name.push_back(my_joint_name);
+      }
+    }
 
-        robot_->model_->transmissions_[i]->propagatePosition(temp_actuator_ptr, temp_joint_ptr);
-        for(int j = 0; j < num_actuators; j++)
-        {
-           temp.actuator_position.push_back(temp_actuator_ptr[j]->state_.position_);
-           temp.actuator_offset.push_back(temp_actuator_ptr[j]->state_.zero_offset_); 
-           temp.joint_position.push_back(temp_joint_ptr[j]->position_);
-        }
+    if (ok)
+    {
+      transmission->propagatePosition(temp_actuator_list, temp_joint_list);
+      for(int j = 0; j < num_joints; j++)
+      {
+	temp_values.joint_position.push_back(temp_joint_list[j]->position_);
+      }
+      for(int j = 0; j < num_actuators; j++)
+      {
+	temp_values.actuator_position.push_back(temp_actuator_list[j]->state_.position_);
+	temp_values.actuator_offset.push_back(temp_actuator_list[j]->state_.zero_offset_);
+      }
+      resp.array.push_back(temp_values);
+    }
 
-	for(int j = 0; j < num_actuators; j++)
-	{
-	   delete temp_joint_ptr[j];
-	   delete temp_actuator_ptr[j];
-	}
-        resp.array.push_back(temp); 
-     }
-  } 
+    for(int j = 0; j < num_actuators; j++)
+    {
+      delete temp_actuator_list[j];
+    }
+    for(int j = 0; j < num_joints; j++)
+    {
+      delete temp_joint_list[j];
+    }
+
+  } // end foreach transmission
+
+  ROS_INFO("SUCCESSSS!!!!");
+
   return true;
 }
 
-}
+
+
+} //end namespace controller
+
 
